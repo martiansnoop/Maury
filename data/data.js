96f0632d86679af99354ab562b87245c6de26a2e
Emily Wenberg <martiansnoop@gmail.com>
2013-08-05 17:45:57 -0700

    Cleans up data file collection

--- data/data.js
+++ data/data.js
@@ -1,119 +1,365 @@
-define({
-  "masterTable" : {
-    "min": 1,
-    "max":100,
-    "items": [
-      {
-        "minor": {
-          "min": 1,
-          "max": 4
-        },
-        "medium": {
-          "min": 1,
-          "max": 10
-        },
-        "major": {
-          "min": 1,
-          "max": 10
-        },
-        "description": "Armor and Shields",
-        "url": "",
-        "nextTable": "armorAndShieldsTable"
+define({"masterTable": {
+  "min": 1,
+  "max": 100,
+  "items": [
+    {
+      "minor": {
+        "min": 1,
+        "max": 4
       },
-
-      {
-        "minor": {
-          "min": 5,
-          "max": 9
-        },
-        "medium": {
-          "min": 11,
-          "max": 20
-        },
-        "major": {
-          "min": 11,
-          "max": 20
-        },
-        "description": "Weapons",
-        "nextTable": "weaponsTable"
-      }
-    ]
-  },
-
-  "table1": {
-    "min": 1,
-    "max": 100,
-    "items": [
-      {
-        "minor": {
-          "min": 1,
-          "max": 50
-        },
-        "medium": {
-          "min": 1,
-          "max": 50
-        },
-        "major": {
-          "min": 1,
-          "max": 50
-        },
-        "description": "table1 item1",
-        "nextTable": "table2"
+      "medium": {
+        "min": 1,
+        "max": 10
       },
-      {
-        "minor": {
-          "min": 51,
-          "max": 100
-        },
-        "medium": {
-          "min": 51,
-          "max": 100
-        },
-        "major": {
-          "min": 51,
-          "max": 100
-        },
-        "description": "table1 item2",
-        "nextTable": "table2"
-      }
-    ]
-  },
-  "table2": {
-    "min": 1,
-    "max": 100,
-    "items": [
-      {
-        "minor": {
-          "min": 1,
-          "max": 50
-        },
-        "medium": {
-          "min": 1,
-          "max": 50
-        },
-        "major": {
-          "min": 1,
-          "max": 50
-        },
-        "description": "table2 item1",
-        "nextTable": null
+      "major": {
+        "min": 1,
+        "max": 10
       },
-      {
-        "minor": {
-          "min": 51,
-          "max": 100
-        },
-        "medium": {
-          "min":51,
-          "max": 100
-        },
-        "major": {
-          "min":51,
-          "max": 100
-        },
-        "description": "table2 item2",
-        "nextTable": null
-      }
-    ]
-  }
-})
+      "description": "Armor and shields",
+      "nextTable": "armorAndShields",
+      "url": "magicItems/armor.html#_armor"
+    },
+    {
+      "minor": {
+        "min": 5,
+        "max": 9
+      },
+      "medium": {
+        "min": 11,
+        "max": 20
+      },
+      "major": {
+        "min": 11,
+        "max": 20
+      },
+      "description": "Weapons",
+      "nextTable": "weapons",
+      "url": "magicItems/weapons.html#_weapons"
+    },
+    {
+      "minor": {
+        "min": 10,
+        "max": 44
+      },
+      "medium": {
+        "min": 21,
+        "max": 30
+      },
+      "major": {
+        "min": 21,
+        "max": 25
+      },
+      "description": "Potions",
+      "nextTable": "potions",
+      "url": "magicItems/potions.html#_potions"
+    },
+    {
+      "minor": {
+        "min": 45,
+        "max": 46
+      },
+      "medium": {
+        "min": 31,
+        "max": 40
+      },
+      "major": {
+        "min": 26,
+        "max": 35
+      },
+      "description": "Rings",
+      "nextTable": "rings",
+      "url": "magicItems/rings.html#_rings"
+    },
+    {
+      "minor": {},
+      "medium": {
+        "min": 41,
+        "max": 50
+      },
+      "major": {
+        "min": 36,
+        "max": 45
+      },
+      "description": "Rods",
+      "nextTable": "rods",
+      "url": "magicItems/rods.html#_rods"
+    },
+    {
+      "minor": {
+        "min": 47,
+        "max": 81
+      },
+      "medium": {
+        "min": 51,
+        "max": 65
+      },
+      "major": {
+        "min": 46,
+        "max": 55
+      },
+      "description": "Scrolls",
+      "nextTable": "scrolls",
+      "url": "magicItems/scrolls.html#_scrolls"
+    },
+    {
+      "minor": {},
+      "medium": {
+        "min": 66,
+        "max": 68
+      },
+      "major": {
+        "min": 56,
+        "max": 75
+      },
+      "description": "Staves",
+      "nextTable": "staves",
+      "url": "magicItems/staves.html#_staves"
+    },
+    {
+      "minor": {
+        "min": 82,
+        "max": 91
+      },
+      "medium": {
+        "min": 69,
+        "max": 83
+      },
+      "major": {
+        "min": 76,
+        "max": 80
+      },
+      "description": "Wands",
+      "nextTable": "wands",
+      "url": "magicItems/wands.html#_wands"
+    },
+    {
+      "minor": {
+        "min": 92,
+        "max": 100
+      },
+      "medium": {
+        "min": 84,
+        "max": 100
+      },
+      "major": {
+        "min": 81,
+        "max": 100
+      },
+      "description": "Wondrous items",
+      "nextTable": "wondrousitems",
+      "url": "magicItems/wondrousItems.html#_wondrous-items"
+    }
+  ]
+}, "armorAndShields": {
+  "min": 1,
+  "max": 100,
+  "items": [
+    {
+      "minor": {
+        "min": 1,
+        "max": 60
+      },
+      "medium": {
+        "min": 1,
+        "max": 5
+      },
+      "major": {},
+      "description": "+1 shield",
+      "nextTable": "+1shield"
+    },
+    {
+      "minor": {
+        "min": 61,
+        "max": 80
+      },
+      "medium": {
+        "min": 6,
+        "max": 10
+      },
+      "major": {},
+      "description": " +1 armor ",
+      "nextTable": "+1 armor "
+    },
+    {
+      "minor": {
+        "min": 81,
+        "max": 85
+      },
+      "medium": {
+        "min": 11,
+        "max": 20
+      },
+      "major": {},
+      "description": "+2 shield ",
+      "nextTable": "+2shield "
+    },
+    {
+      "minor": {
+        "min": 86,
+        "max": 87
+      },
+      "medium": {
+        "min": 21,
+        "max": 30
+      },
+      "major": {},
+      "description": "+2 armor ",
+      "nextTable": "+2armor "
+    },
+    {
+      "minor": {},
+      "medium": {
+        "min": 31,
+        "max": 40
+      },
+      "major": {
+        "min": 1,
+        "max": 8
+      },
+      "description": "+3 shield ",
+      "nextTable": "+3shield "
+    },
+    {
+      "minor": {},
+      "medium": {
+        "min": 41,
+        "max": 50
+      },
+      "major": {
+        "min": 9,
+        "max": 16
+      },
+      "description": "+3 armor ",
+      "nextTable": "+3armor "
+    },
+    {
+      "minor": {},
+      "medium": {
+        "min": 51,
+        "max": 55
+      },
+      "major": {
+        "min": 17,
+        "max": 27
+      },
+      "description": "+4 shield ",
+      "nextTable": "+4shield "
+    },
+    {
+      "minor": {},
+      "medium": {
+        "min": 56,
+        "max": 57
+      },
+      "major": {
+        "min": 28,
+        "max": 38
+      },
+      "description": "+4 armor ",
+      "nextTable": "+4armor "
+    },
+    {
+      "minor": {},
+      "medium": {},
+      "major": {
+        "min": 39,
+        "max": 49
+      },
+      "description": "+5 shield ",
+      "nextTable": "+5shield "
+    },
+    {
+      "minor": {},
+      "medium": {},
+      "major": {
+        "min": 50,
+        "max": 57
+      },
+      "description": "+5 armor ",
+      "nextTable": "+5armor "
+    },
+    {
+      "minor": {},
+      "medium": {},
+      "major": {},
+      "description": "+6 armor/shield1 ",
+      "nextTable": "+6armor/shield1 "
+    },
+    {
+      "minor": {},
+      "medium": {},
+      "major": {},
+      "description": "+7 armor/shield1 ",
+      "nextTable": "+7armor/shield1 "
+    },
+    {
+      "minor": {},
+      "medium": {},
+      "major": {},
+      "description": "+8 armor/shield1 ",
+      "nextTable": "+8armor/shield1 "
+    },
+    {
+      "minor": {},
+      "medium": {},
+      "major": {},
+      "description": "+9 armor/shield1 ",
+      "nextTable": "+9armor/shield1 "
+    },
+    {
+      "minor": {},
+      "medium": {},
+      "major": {},
+      "description": "+10 armor/shield1 ",
+      "nextTable": "+10armor/shield1 "
+    },
+    {
+      "minor": {
+        "min": 88,
+        "max": 89
+      },
+      "medium": {
+        "min": 58,
+        "max": 60
+      },
+      "major": {
+        "min": 58,
+        "max": 60
+      },
+      "description": "Specific armor2 ",
+      "nextTable": "specificarmor2 "
+    },
+    {
+      "minor": {
+        "min": 90,
+        "max": 91
+      },
+      "medium": {
+        "min": 61,
+        "max": 63
+      },
+      "major": {
+        "min": 61,
+        "max": 63
+      },
+      "description": "Specific shield3 ",
+      "nextTable": "specificshield3 "
+    },
+    {
+      "minor": {
+        "min": 92,
+        "max": 100
+      },
+      "medium": {
+        "min": 64,
+        "max": 100
+      },
+      "major": {
+        "min": 64,
+        "max": 100
+      },
+      "description": "Special ability and roll again2,3",
+      "nextTable": "specialability and roll again2,3"
+    }
+  ]
+}});
