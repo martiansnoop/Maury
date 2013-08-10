3b49deac78283f61abbdd1d4e36cccad1eddfc4c
Emily Wenberg <martiansnoop@gmail.com>
2013-08-10 01:59:52 -0700

    Removes need to filter undefined out of result set array

--- js/generator.js
+++ js/generator.js
@@ -2,24 +2,21 @@ define(["./tableManager"], function(tableManager) {
 
   const masterTableId = "masterTable";
 
-  function getItem(itemRarity){
-    return getItemRecursive(masterTableId, itemRarity).filter(function(item) {
-      return item !== undefined;
-    });
-  }
-
   function getItemRecursive(tableId, itemRarity) {
     if(!tableManager.tableExists(tableId))
       return undefined;
 
     var dieRoll = tableManager.roll(tableId);
-    var intermediary = tableManager.lookup(tableId, dieRoll, itemRarity);
+    var intermediaryResult = tableManager.lookup(tableId, dieRoll, itemRarity);
+    var nextResults = getItemRecursive(intermediaryResult.nextTable, itemRarity);
 
-    return [intermediary].concat(getItemRecursive(intermediary.nextTable, itemRarity));
+    return [intermediaryResult].concat(nextResults || []);
   }
 
   return {
-    rollForItem: getItem
+    rollForItem: function (itemRarity) {
+      return getItemRecursive(masterTableId, itemRarity);
+    }
   };
 
 });
