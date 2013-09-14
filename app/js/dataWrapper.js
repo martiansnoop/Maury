d0dc37d377578e12abeeab0308055e8c3d0d99dd
Emily Wenberg <martiansnoop@gmail.com>
2013-09-14 13:54:56 -0700

    Removes demux code from generator

--- app/js/dataWrapper.js
+++ app/js/dataWrapper.js
@@ -3,10 +3,6 @@ define(["./data", "./dice", "jquery"], function(dataTables, d, $) {
   //Private
   const d100 = new d(100);
 
-  function lookupDemux(tableId, dieRoll) {
-    return getEntry(dataTables[tableId].entries, [], dieRoll);
-  }
-
   function getProperty(entry, propChain) {
     var thing = entry;
     propChain.forEach(function (prop) {
@@ -16,8 +12,12 @@ define(["./data", "./dice", "jquery"], function(dataTables, d, $) {
     return thing;
   }
 
-  function getEntry(entries, propChain, dieRoll) {
+  function selectObjectByDieRoll(entries, propChain, dieRoll) {
     var entry = entries.filter(function(entry) {
+      //This is kind of hacky and weird. As things stand now, the min/max of a rollable thing can either
+      //be direct properties on the object, or structured like "minor: {min: 1, max:10}". I think the
+      //best way to fix this would be to change the format of the data to always have the range be an
+      //object itself, so "range: {min: 1, max:42}" if we don't have minor/medium/max for the item
       var min = getProperty(entry, propChain.concat("min"));
       var max = getProperty(entry, propChain.concat("max"));
 
@@ -30,11 +30,15 @@ define(["./data", "./dice", "jquery"], function(dataTables, d, $) {
   //Public
 
   function lookup(tableId, propChain, dieRoll) {
-    var entry = getEntry(dataTables[tableId].entries, propChain, dieRoll);
+    const entriesPropName = "entries";
+    var table =  dataTables[tableId];
+    var entries = table[entriesPropName];
 
-    if(exists(entry.demuxId)) {
-      entry.nextTableId = entry.nextTableId.concat(lookupDemux(entry.demuxId, d100.roll()).appendMe);
+    if(!entries) {
+      entries = selectObjectByDieRoll(table, [], d100.roll())[entriesPropName];
     }
+
+    var entry = selectObjectByDieRoll(entries, propChain, dieRoll);
 
     entry.dieRoll = dieRoll; //for debugging purposes
 
